941b5325c5eaca78c63b650563b5774e61232eab
Sythivo <69376875+Sythivo@users.noreply.github.com>
2022-02-05 01:26:41 -0500

    Update new.lua
    Updated Wrapper

--- make/new.lua
+++ make/new.lua
@@ -1,7 +1,7 @@
 local basic = require(script.Parent:WaitForChild("basic"));
 local include = script.Parent:WaitForChild("@include");
 
-local create = function(InstanceName)
+local create = function(InstanceName : string, wrapper : (Instance) -> (any))
 	local Class;
 	if (include:FindFirstChild(InstanceName)) then
 		Class = require(include:FindFirstChild(InstanceName));
@@ -16,15 +16,28 @@ local create = function(InstanceName)
 	else
 		Class = Instance.new(InstanceName);
 	end
-
+	
+	if (wrapper) then
+		Class = wrapper(Class)
+	end
+	
 	return function(Properties)
 		for i, v in pairs(Properties) do
 			if (typeof(v) == "Instance" and type(i) == "number") then
 				v.Parent = (Class);
 				continue;
 			end
+			if (type(v) == "function" and type(i) == "string") then
+				Class[i]:Connect(v);
+				continue;
+			end
 			Class[i] = v;
 		end
+		
+		if (Class and type(Class["OnInitialized"]) == "table" and type(Class["OnInitialized"]["Fire"]) == "function") then
+			Class.OnInitialized:Fire();
+		end
+		
 		return Class;
 	end
 end
@@ -32,9 +45,7 @@ end
 return function(class)
 	local op_string = (function(class)
 		return (function(paramaters)
-			local object = create(class)(paramaters);
-			
-			return basic.echo(object);
+			return create(class, basic.echo)(paramaters);
 		end);
 	end);
 	local class = class;
