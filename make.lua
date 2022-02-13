c25757e6797849dd0a7e3363c4730f6c3bd88908
Sythivo <69376875+Sythivo@users.noreply.github.com>
2022-02-13 11:34:02 -0500

    Changes

--- make.lua
+++ make.lua
@@ -209,18 +209,18 @@ return {
 	wrap = (script.wrap),
 	types = (script.types),
 	basic = (script.basic),
-	
+
 	new_property = function<T>(default_value : T)
 		local self = ({
 			value = default_value;
 			["@type"] = "property";
 		});
 
-		function self.get() : T
+		function self:get() : T
 			return self.value;
 		end
 
-		function self.set(value : T) : ()
+		function self:set(value : T) : ()
 			assert(not self.value or typeof(self.value) == typeof(value), 
 				("invalid argument #3 (%s expected, got %s)"):format(typeof(self.value), typeof(value))
 			);
